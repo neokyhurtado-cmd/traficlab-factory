8beaf4933b8b9a02ae303ead12be2d7938256950
neokyhurtado-cmd <neokyhurtado@gmail.com>
2026-09-17 18:41:11 -0500

    feat(omh): export integration contract

---
+++ omh_integration/__init__.py
@@ -0,0 +1,23 @@
+from .contract import (
+    CANONICAL_AUTHORITIES,
+    OMH_REF,
+    OMH_VERSION,
+    OMH_WHEEL_SHA256,
+    CanaryObservation,
+    OmhUnitObservation,
+    authority_violations,
+    evaluate_canary,
+    map_omh_state,
+)
+
+__all__ = [
+    "CANONICAL_AUTHORITIES",
+    "OMH_REF",
+    "OMH_VERSION",
+    "OMH_WHEEL_SHA256",
+    "CanaryObservation",
+    "OmhUnitObservation",
+    "authority_violations",
+    "evaluate_canary",
+    "map_omh_state",
+]
